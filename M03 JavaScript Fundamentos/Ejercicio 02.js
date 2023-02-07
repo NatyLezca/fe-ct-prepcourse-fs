e1ad003223444810783a4e709a50fefb6640592f
NatyLezca <imaginare_naty@hotmail.com>
2023-02-06 21:11:11 -0300

    mi primer comit

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,35 +4,48 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
+devolverString ("Hola Loki")
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultadoSuma = x + y;
+   return resultadoSuma;
 }
-
+suma(1,1)
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resultadoResta = x - y;
+   return resultadoResta;
 }
-
+resta(1,3)
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var divide = x/y;
+   return divide;
 }
+divide(20,5)
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var multiplica = x*y;
+   return multiplica;
 }
-
+multiplica(5,4)
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var obtenerResto = x % y;
+   return obtenerResto;
 }
-
+obtenerResto(26,5)
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    devolverString,
---
+++ demo.js
@@ -0,0 +1,104 @@
+
+
+//console.log(dia)
+/* 
+string -> "hola, "Henry, "6,"
+numbers -> 3, -5, -88.5, 45.9
+booleans -> true, false -> es un valor logico
+undefined -> no esta definido
+null ->designada como vacio
+*/
+
+//console.log(8 + 3)
+//var nombreMascota = "Loki";
+// console.log(nombre_mascota.length)
+// var a = 5;
+// var b = 6;
+// var c = 8;
+//console.log(a + b +c);
+
+//console.log(true == 5$);
+
+//console.log(3 === && "hola")
+//code demo js
+//console.log(0 || 6)
+//for(var i = 0 ;  5 ; i++){
+    //console.log(i)
+//}
+// //console.log(comidaRapida.length)
+// var longitudArray = comidaRapida.length
+// //console.log(comidaRapida[longitudArray -1])
+// comidaRapida.push("sandwich")
+// console.log(comidaRapida);
+// comidaRapida.pop()
+// console.log(comidaRapida);
+// comidaRapida.unshift()
+// console.log(comidaRapida);
+// comidaRapida.shift()
+// console.log(comidaRapida);
+
+//var nombre = "Loki"
+//var mi_nombre = "Naty"
+ //var numero = 5.5
+ //var saludo;
+//console.log(dia)
+//var dia = null
+//console.log(6 - 3)
+//console.log(6 + 3)
+//console.log(6 * 3)
+//console.log(6 ** 3)
+//var modulo = 55 % 3;
+//console.log(modulo);
+//var modu = 55 ** 3;
+//console.log(modu);
+//var prece = 5 + 2 * 8 - 3
+//console.log(prece)
+//var a = 5;
+//var b = 6;
+//var c = 8;
+//console.log(a + b + c);
+//console.log(a = b = c);
+//console.log(5 == "5");
+//console.log(true == 5);
+//console.log(3 === 3 && "Loki")
+//console.log(5 > 2 && 3 > 8)
+//console.log(true || true)
+//console.log(true || false)
+//console.log(false || true)
+//console.log(false || false)
+//console.log(5 === 9)
+//var nombre = "Naty"
+//var niego_nombre = !nombre
+//console.log(niego_nombre)
+//var nombre_mascota = "Loki"
+//console.log(nombre_mascota.length)
+
+//var nombre_mascota2 = "Pupi"
+//console.log(nombre_mascota2.length)
+
+//var nombre_mascota3 = "Zapallo"
+//var niegoNombre = !nombre_mascota3
+//console.log(niegoNombre.length)
+//function nombre_funcion(num){
+  // var resultado = num + 5
+   //return resultado
+  
+//}
+//console.log(nombre_funcion(7))
+
+//function nombre_funcion(num){
+    //var resultado = num * 8
+    //return resultado
+   
+ //}
+ //console.log(nombre_funcion(15))
+
+ function semaforo(color){
+    if (color === "rojo") return "No avanzar" 
+    else if(color === "amarillo") return "preste atencion"
+      
+    else if return "avance"
+ }
+ 
+ 
+ 
